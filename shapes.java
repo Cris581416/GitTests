88a0ba0dc91bf7b05ee7c3c25ff2b11211fe9ae3
SnehaG225 <71569216+SnehaG225@users.noreply.github.com>
2020-09-30 13:58:01 -0700

    shapes.java

---
+++ shapes.java
@@ -0,0 +1,29 @@
+public class shapes {
+    
+    
+    
+    public static void drawRectangle (int width){
+        int length = width * 2;
+    
+        for (int x = 1; x <= width; x++) {
+               
+    
+            for(int y = 1; y <= length; y++ ) {
+                System.out.print("b");    
+            }
+            
+            System.out.println("");
+            
+            
+        }
+  
+    }
+    
+
+    
+    
+
+
+
+
+
